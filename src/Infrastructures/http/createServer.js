09cee2db4d7a4fa2d647b674a878871a1f07c933
rizkytm <ritrime97@gmail.com>
2023-06-14 18:46:10 +0700

    hello threads

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -64,6 +64,14 @@ const createServer = async (container) => {
     }),
   });
 
+  server.route({
+    method: 'GET',
+    path: '/threads/test',
+    handler: () => ({
+      value: 'Hello threads!',
+    }),
+  });
+
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
